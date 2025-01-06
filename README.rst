07b3f8bf3b7cf91ca8af66737c36eefbf3c0d0ec
Aditya Bhosale <adityapb@illinois.edu>
2025-01-06 16:55:37 +0530

    Add examples

--- README.rst
+++ README.rst
@@ -5,6 +5,7 @@ A python library for distributed data analytics
 
 Installation
 ------------
+
 1. Install Charm++ non-SMP build
 
 2. Install pyccs. You can find the instructions here - https://github.com/charmplusplus/PyCCS
@@ -18,3 +19,18 @@ Installation
     make -j6
     cd ..
     python setup.py install
+
+Running an example
+------------------
+
+1. First use the ``examples/datagen.py`` script to generate data for the example.::
+
+    python datagen.py
+
+2. Edit the ``examples/Demo.ipynb`` notebook to point to the correct parquet files
+
+3. Run the server from the ``charmpandas/src`` directory::
+    
+    ./charmrun +p4 ./server.out +balancer MetisLB +LBDebug 3 ++server ++server-port 1234
+
+4. Then run the Demo notebook
---
+++ examples/datagen.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from random import shuffle, randint
+
+n = 8000000
+
+first_names = np.array(["A%i" % i for i in range(n)])
+last_names = np.array(["B%i" % i for i in range(n)])
+user_ids = list(range(n))
+ages = [i % 100 for i in range(n)]
+city = ["C%i" % randint(0, 10000) for i in range(n)]
+
+table1 = pd.DataFrame({"first_name" : first_names, "last_name" : last_names,
+                       "user_id" : user_ids, "city" : city})
+
+shuffle(user_ids)
+first_names = first_names[user_ids]
+last_names = last_names[user_ids]
+
+table2 = pd.DataFrame({"first_name" : first_names, "last_name" : last_names,
+                       "age" : ages})
+
+table1.to_parquet("user_ids_large.parquet")
+table2.to_parquet("ages_large.parquet")
